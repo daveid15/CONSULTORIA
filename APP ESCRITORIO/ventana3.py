38f6b46848e044698bb89334704d95d0bd430c21
WestRojas <89225188+WestRojas@users.noreply.github.com>
2024-09-02 17:46:22 -0400

    Interfaz Nueva
    Interfaz local actualizada

--- APP ESCRITORIO/ventana3.py
+++ APP ESCRITORIO/ventana3.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 
-class Ventana1:
+class Ventana3:
     def __init__(self, menu, ventana_principal):
         self.menu = menu
         self.ventana_principal = ventana_principal
-        self.menu.title("Eléctrica")
+        self.menu.title("Electromagnética Invertida")
         self.menu.geometry("800x600")
         self.menu.configure(bg="royalblue4")
 
         #Titulos
-        titulo1 = tk.Label(self.menu, text="Caracterización Eléctrica")
+        titulo1 = tk.Label(self.menu, text="Caracterización Magneto-Electrica INVERTIDA")
         titulo1.place(x=100, y=50)
         #Botones
         btn_volver = tk.Button(self.menu, text="volver a menu", command=self.volver)
