07a87c844ea5a59a102d7c748bddccd6200d1379
Giovanni <giovanni.rojas@cloud.uautonoma.cl>
2024-09-02 17:22:21 -0400

    Interfaz nueva

---
+++ APP ESCRITORIO/ventana1.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+class Ventana1:
+    def __init__(self, menu, ventana_principal):
+        self.menu = menu
+        self.ventana_principal = ventana_principal
+        self.menu.title("Eléctrica")
+        self.menu.geometry("800x600")
+        self.menu.configure(bg="royalblue4")
+
+        #Titulos
+        titulo1 = tk.Label(self.menu, text="Caracterización Eléctrica")
+        titulo1.place(x=100, y=50)
+        #Botones
+        btn_volver = tk.Button(self.menu, text="volver a menu", command=self.volver)
+        btn_volver.place(x=100, y=100)
+
+    def volver(self):
+        self.menu.withdraw()  
+        self.ventana_principal.deiconify()
