ca8423683610e1a8db0e8cfab36dafc44536a4b3
Maximiliano Barria <maximiliano.barria@cloud.uautonoma.cl>
2024-09-05 19:12:46 -0400

    se elimina

--- APP WEB/usuarios/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Persona
-
-class PersonaForm(forms.ModelForm):
-    class Meta:
-        model = Persona
-        fields = ['nombre', 'apellido', 'correo', 'rut', 'perfil']
