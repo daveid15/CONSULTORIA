b6e2c3d091f8cf936bbc3aff4f123db99207721e
Maximiliano Barria <maximiliano.barria@cloud.uautonoma.cl>
2024-11-20 21:38:31 -0300

    migrate, makemigrations

--- APP_WEB/electivo_2023/settings.py
+++ APP_WEB/electivo_2023/settings.py
@@ -89,9 +89,9 @@ DATABASES = {
     'default': {
         'HOST': 'localhost',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'proyecto',
+        'NAME': 'baseconsultoria',
         'USER': 'postgres',
-        'PASSWORD': '12345'
+        'PASSWORD': 'maxi2002'
     }
 }
 
